f5ad09c10c3c87d594b7d5b86621fea103b4d6bb
austingmhuang <gengminghuang@gmail.com>
2021-02-26 05:45:43 -0500

    Works if you download an extension that let's you disable CORS

--- upload-demo/src/components/App.jsx
+++ upload-demo/src/components/App.jsx
@@ -18,14 +18,13 @@ export default function Upload() {
         "http://3.236.97.79:8080/predictions/densenet161",
         {
           method: "POST",
-          mode: "no-cors",
           headers: {
             "Content-Type": "image/jpeg"
           },
           body: data
         }
       );
-      return await response; // an opaque response
+      return await response.json();
     }
     postData(file).then(response => {
       console.log(response);
