26f26f4db606c53cf0e7650f4269503b74212e11
austingmhuang <gengminghuang@gmail.com>
2021-03-09 00:54:44 -0500

    trying to separate out into frames...

--- upload-demo/src/components/Upload.jsx
+++ upload-demo/src/components/Upload.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "../styles/upload.css";
 import mergeImages from "merge-images";
+import extractFramesFromVideo from "../utils/image_gen";
 
 export default function Upload({ ipAddress, port, model }) {
   console.log(ipAddress, model, port);
@@ -20,41 +21,14 @@ export default function Upload({ ipAddress, port, model }) {
   // Call a function (passed as a prop from the parent component) to handle the user-selected file
   const handleChange = event => {
     const file = event.target.files[0];
-    let image;
-    let imgHeight;
-    let imgWidth;
+    let frames;
+
     fileReader.readAsDataURL(file);
     fileReader.onload = () => {
-      const data = fileReader.result;
-      const forImgHeight = new Image();
-      forImgHeight.src = data;
-      forImgHeight.onload = () => {
-        imgWidth = forImgHeight.naturalWidth;
-        imgHeight = forImgHeight.naturalHeight;
-      };
-      image = new FormData();
-      image.append("image-file", data);
+      const videoURL = fileReader.result;
+      extractFramesFromVideo(videoURL).then(res => console.log(res));
+      console.log(frames);
     };
-    postData(file).then(response => {
-      /**
-       * All this code below here is very weird. I will try to write a more concise and "react" way of
-       * accessing the canvas. But for now this works so I will leave it like this...
-       */
-      let canvasElement = document.createElement("canvas");
-      canvasElement.width = imgWidth;
-      canvasElement.height = imgHeight;
-      let canvas = canvasElement.getContext("2d");
-      canvas.fillStyle = "#00FFFF";
-      canvas.textAlign = "center";
-      canvas.textBaseline = "middle";
-      canvas.font = "15px Arial";
-      canvas.fillText(Object.keys(response)[0], imgWidth / 2, imgHeight - 50);
-      textImage.src = canvas.canvas.toDataURL();
-
-      mergeImages([image.get("image-file"), textImage.src]).then(b64 =>
-        setInputImage(b64)
-      );
-    });
   };
 
   async function postData(data) {
---
+++ upload-demo/src/utils/image_gen.js
@@ -0,0 +1,46 @@
+export default async function extractFramesFromVideo(videoURL, fps = 25) {
+  return new Promise(async resolve => {
+    let video = document.createElement("video");
+
+    let seekResolve;
+    video.addEventListener("seeked", async () => {
+      if (seekResolve) seekResolve();
+    });
+
+    video.src = videoURL;
+
+    console.log(video.readyState);
+
+    while (
+      (video.duration === Infinity || isNaN(video.duration)) &&
+      video.readyState < 2
+    ) {
+      await new Promise(r => setTimeout(r, 1000));
+      video.currentTime = 1000000 * Math.random();
+    }
+    let duration = video.duration;
+
+    let canvas = document.createElement("canvas");
+    let context = canvas.getContext("2d");
+    let [w, h] = [video.videoWidth, video.videoHeight];
+    canvas.width = w;
+    canvas.height = h;
+
+    let frames = [];
+    let interval = 1 / fps;
+    let currentTime = 0;
+
+    while (currentTime < duration) {
+      video.currentTime = currentTime;
+      await new Promise(r => seekResolve);
+
+      context.drawImage(video, 0, 0, w, h);
+      let base64ImageData = canvas.toDataURL();
+      frames.push(base64ImageData);
+
+      currentTime += interval;
+    }
+    console.log(frames);
+    resolve(frames);
+  });
+}
